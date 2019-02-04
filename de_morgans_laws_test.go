fd0495480191b7a13ff348a20397dfcde2f17d2f
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2019-02-05 06:16:47 +0900

    updated

--- de_morgans_laws_test.go
+++ de_morgans_laws_test.go
@@ -15,31 +15,58 @@ func TestDeMorgansLaws(t *testing.T) {
 		b2f binary
 	}{
 		{
-			b1f: func(x, y int) int { return ^(x & y) },
-			b2f: func(x, y int) int { return ^x | ^y },
+			b1f: func(x, y int) int { return ^(x & y) }, // ¬(x & y)
+			b2f: func(x, y int) int { return ^x | ^y },  // ¬x | ¬y
 		},
 		{
-			b1f: func(x, y int) int { return ^(x | y) },
-			b2f: func(x, y int) int { return ^x & ^y },
+			b1f: func(x, y int) int { return ^(x | y) }, // ¬(x | y)
+			b2f: func(x, y int) int { return ^x & ^y },  // ¬x & ¬y
 		},
 		{
-			b1f: func(x, _ int) int { return ^(x + 1) },
-			b2f: func(x, _ int) int { return ^x - 1 },
+			b1f: func(x, _ int) int { return ^(x + 1) }, // ¬(x + 1)
+			b2f: func(x, _ int) int { return ^x - 1 },   // ¬x - 1
 		},
 		{
-			b1f: func(x, _ int) int { return ^(x - 1) },
-			b2f: func(x, _ int) int { return ^x + 1 },
+			b1f: func(x, _ int) int { return ^(x - 1) }, // ¬(x - 1)
+			b2f: func(x, _ int) int { return ^x + 1 },   // ¬x + 1
 		},
 		{
-			b1f: func(x, _ int) int { return ^-x },
-			b2f: func(x, _ int) int { return x - 1 },
+			b1f: func(x, _ int) int { return ^-x },   // ¬-x
+			b2f: func(x, _ int) int { return x - 1 }, // x -1
 		},
 		{
-			b1f: func(x, y int) int { return ^(x ^ y) },
-			b2f: func(x, y int) int { return ^x ^ y },
+			b1f: func(x, y int) int { return ^(x ^ y) }, // ¬(x ⊕ y)
+			b2f: func(x, y int) int { return ^x ^ y },   // ¬x ⊕ y
+		},
+		{
+			b1f: func(x, y int) int { return ^(x + y) }, // ¬(x + y)
+			b2f: func(x, y int) int { return ^x - y },   // ¬x - y
+		},
+		{
+			b1f: func(x, y int) int { return ^(x - y) }, // ¬(x - y)
+			b2f: func(x, y int) int { return ^x + y },   // ¬x + y
+		},
+
+		// As an example of the application of these formulas,
+		// ¬(x | –(x + 1)) = ¬x &¬–(x + 1) = ¬x & ((x + 1) – 1) = ¬x & x = 0.
+		{
+			b1f: func(x, y int) int { return ^(x | -(x + 1)) }, // ¬(x | –(x + 1))
+			b2f: func(x, y int) int { return 0 },
+		},
+		{
+			b1f: func(x, y int) int { return ^x & ^-(x + 1) }, // ¬x &¬–(x + 1)
+			b2f: func(x, y int) int { return 0 },
+		},
+		{
+			b1f: func(x, y int) int { return ^x & ((x + 1) - 1) }, // ¬x & ((x + 1) – 1)
+			b2f: func(x, y int) int { return 0 },
+		},
+		{
+			b1f: func(x, y int) int { return ^x & x }, // ¬x & x
+			b2f: func(x, y int) int { return 0 },
 		},
 	} {
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 30; i++ {
 			x := rand.Int()
 			y := rand.Int()
 			if test.b1f(x, y) != test.b2f(x, y) {
